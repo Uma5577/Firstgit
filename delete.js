ddaaf4e771bdc2a113d310ea9fe3d5f0da0de7e9
Uma5689 <spuppy942@gmail.com>
2023-02-02 13:33:38 +0530

    remove item

--- delete.js
+++ delete.js
@@ -54,3 +54,33 @@ itemList.appendChild(li);
 
 
 
+// Remove Item
+function removeItem(e) {
+    if(e.target.classList.contains('delete')) {
+        if(confirm('Are you Sure')) {
+            var li= e.target.parentElement;
+            itemList.removeChild(li);
+        }
+
+    }
+}
+
+//Filter Items
+function filterItems(e) {
+    //convert text to lowercase
+    var text= e.target.value.toLowerCase();
+    //Get lis
+    var items =itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName= item.firstChild.textContent;
+        //description text content
+        var description= item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }
+        else {
+            item.style.display = 'none';
+        }
+    });
+}
