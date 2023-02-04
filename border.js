008164f1c38b50b80cb3f797aeaade079337401d
Uma5689 <spuppy942@gmail.com>
2023-02-04 12:04:05 +0530

    background

--- border.js
+++ border.js
@@ -1,6 +1,18 @@
-const y = document.getElementById('main-header');
-y.style.borderBottom = 'solid 3px #000';
-const z = document.getElementsByClassName('title');
-console.log(z[0]);
-z[0].style.fontWeight = 'bold';
-z[0].style.color = 'green';
+// const y = document.getElementById('main-header');
+// y.style.borderBottom = 'solid 3px #000';
+// const z = document.getElementsByClassName('title');
+// console.log(z[0]);
+// z[0].style.fontWeight = 'bold';
+// z[0].style.color = 'green';
+
+const items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'hello';
+items[2].style.backgroundColor = 'green';
+for (var i = 0; i<items.length;i++){
+    items[i].style.backgroundColor='#f0f0f0';
+}
+for (var i = 0; i<items.length;i++){
+    items[i].style.fontWeight= 'bold';
+}
